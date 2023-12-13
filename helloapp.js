476d42c072cebea157c869e1f5de37412980796e
Швидко Иван <151105537+IvanShvidko@users.noreply.github.com>
2023-12-13 11:38:00 +0800

    Add files via upload

--- helloapp.js
+++ helloapp.js
@@ -1,10 +1,6 @@
 const express = require("express");
   
 const app = express();
-app.get("/", function(_, response){
-      
-    response.send("<h1>Главная страница</h1>");
-});
 app.use("/static", function(request, response){
     response.send(`header: Hello</p>body: Octagon NodeJS Test`);
 });
@@ -14,8 +10,18 @@ app.use("/dynamic", function(request, response){
     const b = request.query.b;
     const c = request.query.c;
     const body = a*b*c/3;
-
-    response.send(`header: Calculated</p>body: ${body}</p>`);
+    if(a==="",b==="",c==="")
+     {
+        response.send(`header: Error</p>body: ${body}</p>`);  
+     }
+     else
+     {
+        const a = request.query.a;
+        const b = request.query.b;
+        const c = request.query.c;
+        const body = a*b*c/3;
+        response.send(`header: Calculated</p>body: ${body}</p>`);  
+     }
 });
  
 app.listen(3000);
