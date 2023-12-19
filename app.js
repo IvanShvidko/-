d9ffcb703ad517cfd8eb273df5a18092e1fd1f7e
Швидко Иван <151105537+IvanShvidko@users.noreply.github.com>
2023-12-20 00:40:51 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.get
+(
+    "/", function(request, response)
+    {
+        response.send("<h1>Главная страница</h1>");
+    }
+);
+
+app.use
+(
+    "/static", function(request, response)
+    {
+        response.send(`header: Hello</p>body: Octagon NodeJS Test`);
+    }
+);
+
+app.use("/dynamic", function(request, response) {
+    let a = parseInt(request.query.a);
+    let b = parseInt(request.query.b);
+    let c = parseInt(request.query.c);
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) 
+    {
+      return response.send(`header: Error</p>\nbody: Error`);
+    }
+
+    if (!a || !b || !c) {
+      return response.send(`header: Error</p>\nbody: Error`);
+    } 
+
+    else 
+    {
+      let result = (a * b * c) / 3;
+      return response.send(`header: Calculated</p>\nbody: ${result}`);
+    }
+    
+  });
+ 
+app.listen(3000,()=>{console.log("Сервер начал прием запросов по адресам: http://localhost:3000/, http://localhost:3000/static, http://localhost:3000/dynamic?a=1&b=2&c=3");});
