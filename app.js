bc9087ea3111e5273fed2c4ce5a575de397fc45d
Швидко Иван <151105537+IvanShvidko@users.noreply.github.com>
2024-01-21 21:18:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.get
-(
-    "/", function(request, response)
-    {
-        response.send("<h1>Главная страница</h1>");
-    }
-);
+app.get("/", function(request, response) {
+  response.send("<h1>Главная страница</h1>");
+});
 
-app.use
-(
-    "/static", function(request, response)
-    {
-        response.send(`header: Hello</p>body: Octagon NodeJS Test`);
-    }
-);
+app.use("/static", function(request, response) {
+  response.send("<p>header: Hello</p><p>body: Octagon NodeJS Test</p>");
+});
 
 app.use("/dynamic", function(request, response) {
-    let a = parseInt(request.query.a);
-    let b = parseInt(request.query.b);
-    let c = parseInt(request.query.c);
+  let a = parseInt(request.query.a);
+  let b = parseInt(request.query.b);
+  let c = parseInt(request.query.c);
 
-    if (isNaN(a) || isNaN(b) || isNaN(c)) 
-    {
-      return response.send(`header: Error</p>\nbody: Error`);
-    }
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    return response.send("<p>header: Error</p><p>body: Error</p>");
+  }
 
-    if (!a || !b || !c) {
-      return response.send(`header: Error</p>\nbody: Error`);
-    } 
+  if (!a || !b || !c) {
+    return response.send("<p>header: Error</p><p>body: Error</p>");
+  } else {
+    let result = (a * b * c) / 3;
+    return response.send(`<p>header: Calculated</p><p>body: ${result}</p>`);
+  }
+});
 
-    else 
-    {
-      let result = (a * b * c) / 3;
-      return response.send(`header: Calculated</p>\nbody: ${result}`);
-    }
-    
-  });
- 
-app.listen(3000,()=>{console.log("Сервер начал прием запросов по адресам: http://localhost:3000/, http://localhost:3000/static, http://localhost:3000/dynamic?a=1&b=2&c=3");});
+app.listen(3000, () => {
+  console.log("Сервер начал прием запросов по адресам: http://localhost:3000/, http://localhost:3000/static, http://localhost:3000/dynamic?a=1&b=2&c=3");
+});
